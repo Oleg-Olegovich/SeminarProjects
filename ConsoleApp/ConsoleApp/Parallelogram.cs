26561fa017a8e7b664cf206ae9577dc15f52281e
Olek_Olegovich <olek.olegovich@gmail.com>
2020-11-14 13:41:45 +0300

    add_proj_14_11

---
+++ ConsoleApp/ConsoleApp/Parallelogram.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Parallelogram : Shape
+    {
+        // 0 - 2 и 1 - 3 параллельные. 
+        private double[] _sides = new double[4];
+
+        private double _angle;
+
+        public override double Perimeter
+            => 2 * (_sides[0] + _sides[1]);
+
+        public override double Square
+            => _sides[0] * _sides[1] * Math.Sin(_angle);
+
+        public Parallelogram()
+        {
+            Name = "Paraallelogram";
+            Console.WriteLine("Введите радиус круга.");
+            Console.WriteLine("Введите длины 2-х смежных сторон параллелограмма (положительные вещественные числа).");
+            _sides[0] = _sides[2] = GetDoubleInput(0, 100000000);
+            _sides[1] = _sides[3] = GetDoubleInput(0, 100000000);
+            Console.WriteLine("Введите значение острого угла");
+            _angle = GetDoubleInput(0, 1000000);
+        }
+
+        public Parallelogram(double a, double b, double alpha)
+        {
+            Name = "Paraallelogram";
+            _sides[0] = _sides[2] = a;
+            _sides[1] = _sides[3] = b;
+            _angle = alpha;
+        }
+
+        public static Parallelogram Generate()
+        {
+            return new Parallelogram(GetRandomNumber(Double.Epsilon, 100000000),
+                GetRandomNumber(Double.Epsilon, 100000000),
+                GetRandomNumber(Double.Epsilon, 1000000));
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        private static List<Shape> _shapes;
+
+        static void Input<T>() where T : Shape, new()
+        {
+            T shape;
+            Console.WriteLine("Выберите способ ввода.");
+            Console.WriteLine("Нажмите \"0\" для ручного ввода или любую другую клавишу для генерации.");
+            if (Console.ReadKey().KeyChar == '0')
+            {
+                shape = new T();
+            }
+            else
+            {
+                shape = (T)Shape.Generate<T>();
+            }
+            _shapes.Add(shape);
+        }
+
+        static void ShapesInitialization()
+        {
+            _shapes = new List<Shape>();
+            do
+            {
+                Console.WriteLine(Environment.NewLine + "Выберите тип фигуры:");
+                Console.WriteLine("\"0\" - треугольник;");
+                Console.WriteLine("\"1\" - прямоугольник;");
+                Console.WriteLine("\"2\" - круг;");
+                Console.WriteLine("\"3\" - параллелограм.");
+                char type = Console.ReadKey().KeyChar;
+                while (type < '0' || type > '3')
+                {
+                    Console.WriteLine("Некорректный ввод, попробуйте ещё раз:");
+                    type = Console.ReadKey().KeyChar;
+                }
+                Console.WriteLine(Environment.NewLine + "Нажмите \"0\", если хотите сгенерировать множество фигур.");
+                Console.WriteLine("Иначе нажмите любую клавишу.");
+                if (Console.ReadKey().KeyChar == '0')
+                {
+                    Console.WriteLine();
+                    int count = Shape.GetIntInput(1, 100);
+                    for (int i = 0; i < count; ++i)
+                    {
+                        _shapes.Add(type switch
+                        {
+                            '0' => Triangle.Generate(),
+                            '1' => Rectangle.Generate(),
+                            '2' => Circle.Generate(),
+                            _ => Parallelogram.Generate()
+                        });
+                    }
+                }
+                else
+                {
+                    Console.WriteLine();
+                    switch (type)
+                    {
+                        case '0':
+                            Input<Triangle>();
+                            break;
+                        case '1':
+                            Input<Rectangle>();
+                            break;
+                        case '2':
+                            Input<Circle>();
+                            break;
+                        default:
+                            Input<Parallelogram>();
+                            break;
+                    }
+                }
+                Console.WriteLine(Environment.NewLine + "Нажмите \"Escape\", чтобы прекратить инициализацию фигур.");
+                Console.WriteLine("Иначе нажмите любую клавишу.");
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+
+        static void Main(string[] args)
+        {
+            ShapesInitialization();
+            string file = "shapes.json";
+            string output = "";
+            foreach (var shape in _shapes)
+            {
+                output += JsonConvert.SerializeObject(shape) + Environment.NewLine;
+            }
+            File.WriteAllText(file, output);
+        }
+    }
+}
