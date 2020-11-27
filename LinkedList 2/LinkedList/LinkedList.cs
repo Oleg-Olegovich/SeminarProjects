5460e30b187a2282e0e8cd89a912fed1527f9a90
Olek_Olegovich <olek.olegovich@gmail.com>
2020-11-27 18:24:36 +0300

    implemented_custom_list

---
+++ LinkedList 2/LinkedList/LinkedList.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Data.Common;
+
+namespace LinkedList
+{
+    /// <summary>
+    /// Необходимо реализовать унаследованный классом LinkedList интерфейс 
+    /// (реализовать = написать все объявленные в нем методы)
+    /// 
+    /// Сразу поясняю T - не равно Node!
+    /// Другие члены - поля, свойства, методы и пр. - можно добавлять на ваше усмотрение!
+    /// </summary>
+    class LinkedList<T> : ICustomCollection<T>
+    {
+        private Node<T> _head;
+
+        private Node<T> _tail;
+
+        public T this[int index]
+        {
+            get => GetNode(index).Item;
+            set => GetNode(index).Item = value;
+        }
+
+        public int Length { get; set; }
+
+        private Node<T> GetNode(int index)
+        {
+            if (index < 0 || index >= Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            var node = _head;
+            for (int i = 0; i < index; ++i)
+            {
+                node = node.Next;
+            }
+            return node;
+        }
+
+        public void Add(T item)
+        {
+            var node = new Node<T>(item);
+            if (_head == null)
+            {
+                _head = node; 
+            }
+            else
+            {
+                node.Previous = _tail;
+                _tail.Next = node;
+            }
+            _tail = node;
+            ++Length;
+        }
+
+        public void AddAsTheFirst(T data)
+        {
+            var node = new Node<T>(data);
+            if (Length == 0)
+            {
+                _head = _tail = node;
+            }
+            else
+            {
+                _head.Previous = node;
+                node.Next = _head;
+                _head = node;
+            }
+            ++Length;
+        }
+
+        public bool Contains(T item)
+        {
+            var node = _head;
+            while (node != null)
+            {
+                if (node.Item.Equals(item))
+                {
+                    return true;
+                }
+                node = node.Next;
+            }
+            return false;
+        }
+
+        public bool IsEmpty()
+            => Length > 0;
+
+        public void Remove(int index)
+        {
+            var node = GetNode(index);
+            if (node.Previous == null)
+            {
+                node.Next.Previous = null;
+                _head = node.Next;
+            }
+            else
+            {
+                node.Previous.Next = node.Next;
+                node.Next.Previous = node.Previous;
+            }
+            --Length;
+        }
+
+        public override string ToString()
+        {
+            string stringOfItems = "";
+            var node = _head;
+            while (node != null)
+            {
+                stringOfItems += $"{node.Item.ToString()}; ";
+                node = node.Next;
+            }
+            return $"{Length} items: [{stringOfItems}]";
+        }
+    }
+  
+
+    class Node<T>
+    {
+        public T Item { get; set; }
+
+        public Node<T> Previous { get; set; }
+
+        public Node<T> Next { get; set; }
+
+        public Node(T item)
+        {
+            Item = item;
+        }
+    }
+}
---
+++ LinkedList 2/LinkedList/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        /// <summary>
+        /// Необходимо реализовать коллекцию LinkedList, тогда этот метод должен успешно сработать.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            ICustomCollection<int> myList = new LinkedList<int>();
+            Console.WriteLine(myList);
+            for(int i = 0; i < 10; i++)
+            {
+                myList.Add(i);
+            }
+            Console.WriteLine(myList);
+            if(myList.Length != 10)
+            {
+                Console.WriteLine($"ОШИБКА, {myList.Length} не равно 10!");
+                return;
+            }
+            myList.Remove(0);
+            if (myList.Length != 9)
+            {
+                Console.WriteLine($"ОШИБКА, {myList.Length} не равно 9!");
+                return;
+            }
+            for(int i = 0; i < 9; i++)
+            {
+                if(myList[i] != i + 1)
+                {
+                    Console.WriteLine($"ОШИБКА, {myList[i]} не равно {i + 1}!");
+                    return;
+                }
+            }
+            myList[3] = 100;
+            while (!myList.IsEmpty())
+            {
+                myList.Remove(0);
+            }
+            Console.WriteLine(myList);
+            if (!myList.IsEmpty())
+            {
+                Console.WriteLine($"ОШИБКА, лист должен был быть пуст!");
+                return;
+            }
+            Console.WriteLine("Поздравляем, ваш лист прошел несколько простых тестов! Возможно он упадет на других, но пока все ок!");
+            Console.Read();
+        }
+    }
+}
